a5eaddf15562dd06b6000bc2861aa5605a4f0be1
officialpuretalent <siphumelelotqwabe@gmail.com>
2022-07-03 20:39:16 +0200

    Added fake blog data, and used useState hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        { title: "My new website", body: "lorem ipsum...", "author": "Sbanisethu", id: 1 },
+        { title: "Welcome Party!", body: "lorem ipsum...", "author": "Talent", id: 2 },
+        { title: "Web dev top tips", body: "lorem ipsum...", "author": "Sbanisethu", id: 3 }
+    ]);
+    
     return (
         <div className="home">
-            <h1>Home Component</h1>
+            {blogs.map((post) => (
+                // The Id must be unique for each post otherwise
+                // whatever we are trying to do, won't work.
+                <div className="post-preview" key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>Written by: {post.author}</p>
+                </div>
+            ))}
         </div> 
     );
 }

1a28d18babad579a47bf723f024453b8f2b3da45
officialpuretalent <siphumelelotqwabe@gmail.com>
2022-07-04 18:58:28 +0200

    Added handleDelete as a prop

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,4 +1,5 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
+
     return (
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -6,6 +7,7 @@ const BlogList = ({ blogs, title }) => {
                 <div className="post-preview" key={post.id}>
                     <h2>{post.title}</h2>
                     <p>Written by: {post.author}</p>
+                    <button onClick={() => handleDelete(post.id)}>Delete Blog</button>
                 </div>
             ))}
         </div>

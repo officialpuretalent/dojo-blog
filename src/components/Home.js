b36db2279b3455c50726e825988b135ac7f8d12a
officialpuretalent <siphumelelotqwabe@gmail.com>
2022-07-04 18:37:20 +0200

    Remove post preview logic to BlogList component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
@@ -9,14 +10,7 @@ const Home = () => {
     
     return (
         <div className="home">
-            {blogs.map((post) => (
-                // The Id must be unique for each post otherwise
-                // whatever we are trying to do, won't work.
-                <div className="post-preview" key={post.id}>
-                    <h2>{post.title}</h2>
-                    <p>Written by: {post.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs"/>
         </div> 
     );
 }
